9f27e625be68c5d3db431343a77e514054eae705
saiprasad <saiprasad.hegde@codecraft.co.in>
2024-03-28 15:59:55 +0530

    Re-implemented isPrime() and implemented generatePrimeSeries() using isPrime()

--- prime.js
+++ prime.js
@@ -1,54 +1,99 @@
-/*This file contains the isPrime() function which throws an exception based on pre-conditions and post-conditions 
-You can import isPrime() through require function */
+import assert from "assert";
 
+function isPrime(num) {
+  //pre-conditions for isPrime
+  assert(typeof num === "number" && num > 1, "must be a number greater than 1");
 
-//user-defined assertion function
-const assert=(condition,message)=>{
-    if(!condition){
-        console.log("Oops!! the input is invalid, here is the exception thrown to describe why it is invalid");
-        throw new Error(message);
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
     }
+  }
+  return true;
 }
 
-
-const isPrime=(number)=>{
-    //Pre-condition
-    if(assert(number>=2 && typeof number === 'number',"Input must be a number and greater than 2" )){
-        return;
-    }
-    
-
-    //validation of prime number
-    for(let i=2;i<=Math.sqrt(number);i++){
-        
-            assert(number%i!==0,`${number} is not a prime number`);
-        
-         }
-   
-    
-    
-        return true;
-    
-
-    
+//post-conditions for isPrime
+try {
+  assert(isPrime(3), "isPrime(3) must return true");
+} catch (error) {
+  console.error(error.message);
 }
 
+try {
+  assert(isPrime(5), "isPrime(5) must return true");
+} catch (error) {
+  console.error(error.message);
+}
 
-//post conditions
-assert(isPrime(11))
-assert(isPrime(5))
-assert(isPrime(0))
-assert(isPrime('a'))
-assert(isPrime(9))
+try {
+  assert(isPrime("a") === false, "isPrime('a') must return false");
+} catch (error) {
+  console.error(error.message);
+}
 
+try {
+  assert(isPrime(9) === false, "isPrime(9) must return false");
+} catch (error) {
+  console.error(error.message);
+}
 
+function generatePrimeSeries(count) {
+  //pre-conditions for generatePrieSeries
+  assert(typeof count === "number", "argument must be a number");
+  assert(count > 0, "argument must be greater than 0");
 
+  const series = [];
+  let num = 2;
+  while (series.length < count) {
+    try {
+      if (isPrime(num)) {
+        series.push(num);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+    num++;
+  }
+  return series;
+}
 
+//post-conditions for generatePrimeSeries
+try {
+  assert.deepStrictEqual(
+    generatePrimeSeries(2),
+    [2, 3],
+    "generatePrimeSeries(2) should return [2, 3]"
+  );
+} catch (error) {
+  console.error(error.message);
+}
 
-module.exports={isPrime,assert};
+try {
+  assert.deepStrictEqual(
+    generatePrimeSeries("hello"),
+    [],
+    'generatePrimeSeries("hello") should return an empty array'
+  );
+} catch (error) {
+  console.error(error.message);
+}
 
+try {
+  assert.deepStrictEqual(
+    generatePrimeSeries(-1),
+    [],
+    "generatePrimeSeries(-1) should return an empty array"
+  );
+} catch (error) {
+  console.error(error.message);
+}
 
-
-
-
-
+try {
+  assert.deepStrictEqual(
+    generatePrimeSeries(4),
+    [2, 3, 5, 7],
+    "generatePrimeSeries(4) should return [2, 3, 5, 7]"
+  );
+} catch (error) {
+  console.error(error.message);
+}
