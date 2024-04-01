9dcf073523c1539a105b55494311b2e8bb0917ea
saiprasad <saiprasad.hegde@codecraft.co.in>
2024-04-01 14:43:12 +0530

    modified the logic to verify isPrime for large numbers

--- prime.js
+++ prime.js
@@ -3,9 +3,15 @@ import assert from "assert";
 function isPrime(num) {
   //pre-conditions for isPrime
   assert(typeof num === "number" && num > 1, "must be a number greater than 1");
+  if (num <= 3) return true;
 
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
+  // Check if num is divisible by 2 or 3
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  // Iterate through potential factors of num
+  // Starting from 5, we only need to check up to the square root of num
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
       return false;
     }
   }
@@ -29,6 +35,12 @@ try {
 
 try {
   assert(isPrime(3), "isPrime(3) must return true");
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  assert(isPrime(67280421310721), "isPrime(3) must return true");
 } catch (error) {
   console.error(error.message);
 }
